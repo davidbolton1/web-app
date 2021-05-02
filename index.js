fdf9d4cd9e638ec6380d6758069f8f1b056102df
David Bolton <39308057+davidbolton1@users.noreply.github.com>
2021-05-02 00:11:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,14 @@ app.use(
 app.use(
  auth({
    secret: SESSION_SECRET,
+   authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+     scope: "openid profile email read:reports",
+   },
  })
 );
 
